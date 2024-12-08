342caf56c522ad78fbffab797cce75853d950aff
Vinicius Oliveira <viniciuz6460@gmail.com>
2024-12-08 20:11:54 -0300

    showing schedules

---
+++ src/services/schedule-fetch-by-day.js
@@ -0,0 +1,17 @@
+import { apiConfig } from "./api-config.js"
+import dayjs from "dayjs"
+
+export async function scheduleFetchByDay({date}){
+    try {
+        const response = await fetch(`${apiConfig.baseUrl}/schedules`)
+
+        const data = await response.json()
+
+        const dailySchedules = data.filter((schedule) => dayjs(date).isSame(schedule.when, "day"))
+
+        return dailySchedules
+    } catch (error) {
+        console.log(error)
+        alert("Não foi possível buscar os agendamentos do dia selecionado.")
+    }
+}
